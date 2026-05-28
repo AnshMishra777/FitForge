c17a1002840961d225bb48be010696cb66208b21
AnshMishra777 <anshranjanmishra66@gmail.com>
2026-05-28 22:52:22 +0530

    Fixed api url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api', // matches your vite proxy
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('ff_token')
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
   return config
 })
 
